a1cd501f83153d46f3922f4bf16bc3e11224a653
april3005 <55992572+april3005@users.noreply.github.com>
2019-10-05 19:10:26 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,34 @@
-package com.company;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        String[] names = new String [3];
-        names[0] = "Mike";
-        names[1] = "Jack";
-        names[2] = "Billy";
+            String[] names = new String[3];
+            names[0] = "Mike";
+            names[1] = "Jack";
+            names[2] = "Billy";
+            names = Arrays.copyOf(names, names.length + 1);
+            names[3]= "Sam";
 
-        int greeting = 1;
-        switch (greeting) {
-            case 1:
-                System.out.println("Good morning " + names[0]);
+        for (int i = 0; i < names.length; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("Good morning " + names[0]);
+                    break;
+                case 1:
+                    System.out.println("Good afternoon " + names[1]);
+                    break;
+                case 2:
+                    System.out.println("Good evening " + names[2]);
+                    break;
+                case 3:
+                    System.out.println("Good night " + names[3]);
+                    break;
+        }
+        
 
-            case 2:
-                System.out.println("Good afternoon " + names[1]);
-
-             case 3:
-                System.out.println("Good evening " + names[2]);
-                
+            }
 
         }
-
-
-
-
     }
-
-
-}
